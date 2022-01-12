f342e2c2a463c15c13787a772af7b740510dced6
Robert Bullfrog <williampikus@gmail.com>
2022-01-11 23:48:05 -0800

    Added end game function, and feedback for answers chosen

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var timeElement = $("#time");
 var startButton = $("#start-button")
-var viewHighscroe = $("#view-hs")
+var viewHighscore = $("#view-hs")
 var tutorialText = $("#tutorial-text")
+var container = $("#container-element")
 var mainText = $("#main-text")
 var olList = $("#list")
 var answer1 = $("#quiz-btn1")
@@ -16,43 +17,11 @@ var questionNum = 0;
 
 var questions = ["Commonly used data types DO NOT include:", "The condition in an if/else statment is enclosed within_________.", "Arrays in JavaScript can be used to store ________.", "String values must be enclosed within ________ when being assigned to variables", "A very useful tool used during development and debugging for printing content to the debugger is:"]
 var answersArray = [["strings", "booleans", "alerts", "numbers",], ["quotes", "curly brackets", "parentheses", "square brackets"], ["numbers and strings", "other arrays", "booleans", "all of the above"], ["commas", "curly brackets", "quotes", "parentheses"],["JavaScript", "terminal/bash", "for loops", "console.log"] ]
-var answers = {
-    q1 : {
-        a1 : "strings",
-        a2 : "booleans",
-        ca : "alerts",
-        a4 : "numbers"
-    },
-    q2 : {
-        a1 : "quotes",
-        a2 : "curly brackets",
-        ca : "parentheses",
-        a4 : "square brackets"
-    },
-    q3 : {
-        a1 : "numbers and strings",
-        a2 : "other arrays",
-        a3 : "booleans",
-        ca : "all of the above"
-    },
-    q4 : {
-        a1 : "commas",
-        a2 : "curly brackets",
-        ca : "quotes",
-        a4 : "parentheses"
-    },
-    q5 : {
-        a1 : "JavaScript",
-        a2 : "terminal/bash",
-        a3 : "for loops",
-        ca : "console.log"
-    }
-}
 
 // The startGame function is called when the start button is clicked
 function startGame() {
     timerCount = 60;
-    // Prevents start button from being clicked when round is in progress
+
     startTimer()
     setUp()
     populatePage()
@@ -97,22 +66,54 @@ function populatePage() {
         $("#quiz-btn3").text(answersArray[questionNum][2])
         $("#quiz-btn4").text(answersArray[questionNum][3])
     }
+    else if (questionNum == 5){
+        endGame()
+    }
     
 }
 
 function isCorrect(event) {
     var btnClicked = $(event.target);
-
-    if (btnClicked.text == answers.q1.ca.text){
+    console.log(btnClicked.text())
+    
+    if (btnClicked.text() == (answersArray[0][2])){
         score += 10
         questionNum ++
+        container.append('<h2 id="reponse">Correct!</h2>')
     } 
+    else if (btnClicked.text() == (answersArray[1][2])){
+        score += 10
+        questionNum ++
+        container.append('<h2 id="reponse">Correct!</h2>')
+    }
+    else if (btnClicked.text() == (answersArray[2][3])){
+        score += 10
+        questionNum ++
+        container.append('<h2 id="reponse">Correct!</h2>')
+    }
+    else if (btnClicked.text() == (answersArray[3][2])){
+        score += 10
+        questionNum ++
+        container.append('<h2 id="reponse">Correct!</h2>')
+    }
+    else if (btnClicked.text() == (answersArray[4][3])){
+        score += 10
+        questionNum ++
+        container.append('<h2 id="reponse">Correct!</h2>')
+    }
     else{
         timerCount = timerCount - 10
         questionNum ++
+        container.append('<h2 id="reponse">Wrong!</h2>')
+        
     }
-    populatePage()
+
+    setTimeout(function () {
+        container.children('h2').remove()
+        populatePage()
+    }, 3000);
 }
+
 
 function setUp(){
     // remove p tag
@@ -124,6 +125,20 @@ function setUp(){
     olList.append('<button class="button-style button-clicked" id="quiz-btn2">answer1</button>')
     olList.append('<button class="button-style button-clicked" id="quiz-btn3">answer1</button>')
     olList.append('<button class="button-style button-clicked" id="quiz-btn4">answer1</button>')
+}
+
+function highScore(){
+
+}
+
+function endGame() {
+    answer1.remove()
+    answer2.remove()
+    answer3.remove()
+    answer4.remove()
+    mainText.text("All done")
+    container.append('<p>Your final score is</p>')
+
 }
 
 
@@ -153,4 +168,5 @@ function startTimer() {
 
 // Attach event listener to start button to call startGame function on click
 startButton.on("click", startGame);
+viewHighscore.on("click", highScore);
 
